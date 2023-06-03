41d3f174ce8c66d06afedf93871ceeff212cb696
sooop <88355765+Peooop@users.noreply.github.com>
2023-06-03 21:03:43 +0800

    Update control.sh

--- control.sh
+++ control.sh
@@ -3,7 +3,7 @@
 MODDIR="$(dirname "$(readlink -f "$0")")"
 
 while true; do # 循环
-    if [ -f "./disable" ]; then
+    if ls /data/adb/modules/Alist/ | grep -q "disable"; then
         if pgrep -f 'alist' >/dev/null; then
             echo "开关控制$(date "+%Y-%m-%d %H:%M:%S") 进程已存在，正在关闭 ..."
             pkill alist # 关闭进程
