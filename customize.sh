814a0485a5fd6f02a4ede934d3bb5d3924322edb
sooop <88355765+Peooop@users.noreply.github.com>
2023-06-03 21:07:08 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,26 +1,15 @@
 #!/system/bin/sh
-# busybox的路径地址
-busybox="/data/adb/magisk/busybox"
-ui_print " -------------------------- "
-ui_print " ------ 安装中，请稍等..."
+MODDIR=${0%/*}
+
+ui_print "安装中，请稍等..."
 sleep 1
+ui_print "AList是一款提供多个储存挂载服务的开源软件"
+ui_print "你可以挂载世面上大部分网盘如: 阿里云盘 百度网盘 蓝奏云等 使其便于管理"
+ui_print "Alist支持webdav协议使其可以桥接世面大部分观影阅读软件"
+ui_print "你可以尽情享受原画观影的快乐"
 
-ui_print " ------ 官方最新版本""`($busybox wget -q -O- "https://api.github.com/repos/alist-org/alist/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')`"
-
-alistVersion="$(cat module.prop | grep "version=")"
-ui_print " ------ 模块版本$(echo ${alistVersion##*=})"
-
-ui_print " ------ 默认开机自启alist进程"
-
-ui_print " ------ 开机后执行/data/adb/modules/Alist/目录内start.sh文件"
-
-ui_print " ------ update.sh、control.sh文件提供自动更新、开关控制启动停止"
-
-ui_print " ------ 更新间隔为2小时检查一次，可自行修改update.sh文件"
-
-ui_print " ------ 网页后台地址:127.0.0.1:5244 账户 admin 密码 5244"
+ui_print "网页后台登录地址:http://127.0.0.1:5244 账户 admin 密码 5244"
 
 sleep 1
-ui_print " ------ 安装已完成，请重启 ↘"
-ui_print " -------------------------- "
+ui_print "安装已完成，请重启 ↘"
 
