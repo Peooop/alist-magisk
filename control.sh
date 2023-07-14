92c809c59fb534a50e349085aab64b371f4995dc
sooop <88355765+Peooop@users.noreply.github.com>
2023-07-14 12:33:19 +0800

    更新 control.sh

--- control.sh
+++ control.sh
@@ -2,19 +2,11 @@
 
 MODDIR="$(dirname "$(readlink -f "$0")")"
 
-while true; do # 循环
+while true; do
     if ls /data/adb/modules/Alist/ | grep -q "disable"; then
-        if pgrep -f 'alist' >/dev/null; then
-            echo "开关控制$(date "+%Y-%m-%d %H:%M:%S") 进程已存在，正在关闭 ..."
-            pkill alist # 关闭进程
-        fi
+        pkill -f 'alist'
     else
-        if pgrep -f 'alist' >/dev/null; then
-            echo "开关控制$(date "+%Y-%m-%d %H:%M:%S") 进程已存在"
-        else
-            echo "开关控制$(date "+%Y-%m-%d %H:%M:%S") 进程不存在，启动 ..."
-            $MODDIR/bin/alist server --data "$MODDIR/data" &
-        fi
+        pgrep -f 'alist' >/dev/null || $MODDIR/bin/alist server --data "$MODDIR/data" &
     fi
-    sleep 3s # 暂停3秒后再次执行循环
+    sleep 3
 done
