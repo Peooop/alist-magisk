9b0379dfcdd57c1be4e80e86c805f6516ca17a42
sooop <88355765+Peooop@users.noreply.github.com>
2023-07-15 21:21:39 +0800

    Update control.sh

--- control.sh
+++ control.sh
@@ -3,10 +3,10 @@
 MODDIR="$(dirname "$(readlink -f "$0")")"
 
 while true; do
-    if ls /data/adb/modules/Alist/ | grep -q "disable"; then
+    if [ -f "/data/adb/modules/Alist/disable" ]; then
         pkill -f 'alist'
     else
         pgrep -f 'alist' >/dev/null || $MODDIR/bin/alist server --data "$MODDIR/data" &
     fi
-    sleep 3
+    sleep 5
 done
