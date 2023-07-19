162548143a1921ea57f78ec049176c4d1a557373
sooop <88355765+Peooop@users.noreply.github.com>
2023-07-19 22:03:08 +0800

    更新 update.sh

--- update.sh
+++ update.sh
@@ -47,16 +47,8 @@ download_and_extract() {
     "${BUSYBOX}" wget -O "${MODDIR}/tmp/${Alist_file}" "${URL}${Alist_file}"
     chmod 755 "${MODDIR}/tmp/${Alist_file}"
     "${BUSYBOX}" ar -p "${MODDIR}/tmp/${Alist_file}" data.tar.xz > "${MODDIR}/tmp/data.tar.xz" && "${BUSYBOX}" tar -xf "${MODDIR}/tmp/data.tar.xz" -C "${MODDIR}/tmp"
-}
-
-# 复制更新后的文件到对应目录下
-copy_files() {
     mv -f "${MODDIR}/tmp/data/data/com.termux/files/usr/bin/alist" "${MODDIR}/bin/alist"
     chmod 755 "${MODDIR}/bin/alist"
-}
-
-# 清理临时文件
-cleanup() {
     rm -r "${MODDIR}/tmp"
 }
 
@@ -67,17 +59,46 @@ version_ge() {
 
 # 更新列表并重启进程
 update_and_restart() {
-    echo "[$(date "+%Y-%m-%d] [%H:%M:%S")] 更新后v${version}版本" >> "${MODDIR}/log.log"
+    echo "[$(date "+%Y-%m-%d] [%H:%M:%S")] v${version} after the update" >> "${MODDIR}/log.log"
     sed -i "s/^version=.*/version=v${version}/g" "${MODDIR}/module.prop"
     if pgrep -f 'alist' >/dev/null; then
         pkill alist 
     fi
     "${MODDIR}/bin/alist" server --data "${MODDIR}/data" &
 }
+# 更新检测
+check_and_update_version() {
+    if version_ge "${url_version}" "${version}"; then
+        echo "[$(date "+%Y-%m-%d] [%H:%M:%S")] v${version} latest version" >> "${MODDIR}/log.log"
+        sed -i "s/^version=.*/version=v${version}/g" "${MODDIR}/module.prop"
+    else
+        echo "[$(date "+%Y-%m-%d] [%H:%M:%S")] v${version}，updating ..." >> "${MODDIR}/log.log"
+        download_and_extract
+        
+        max_retries=3
+        retry_count=0
+
+        while true; do
+            version=$("${MODDIR}/bin/alist" version | awk '/^Version:/ {print $2}')
+            if version_ge "${url_version}" "${version}"; then
+                update_and_restart
+                break
+            fi
+
+            sleep 5s
+            ((retry_count++))
+
+            if [[ ${retry_count} -eq ${max_retries} ]]; then
+                handle_failed_update
+                break
+            fi
+        done
+    fi
+}
 
 # 处理更新失败
 handle_failed_update() {
-    echo "[$(date "+%Y-%m-%d] [%H:%M:%S")] 更新失败" >> "${MODDIR}/log.log"
+    echo "[$(date "+%Y-%m-%d] [%H:%M:%S")] update failed !" >> "${MODDIR}/log.log"
 }
 
 # 查找并设置busybox路径
@@ -90,43 +111,29 @@ while true; do
         continue
     fi
     
-    # 获取最新版本号
-    url_version="$("${BUSYBOX}" wget -q --no-check-certificate -O - "${URL}" | grep -o 'alist_[^"]*' | sed 's/alist_//' | grep '_'"${ARCH}"'.deb$' | sed 's/_'"${ARCH}"'\.deb//' | tail -n 1)"
-    
-    # 获取当前Alist版本号
-    version="$("${MODDIR}/bin/alist" version | awk '/^Version:/ {print $2}')"
+    # 获取最新版本号（尝试3次）
+    retry_times=0
+    while [ $retry_times -lt 3 ]; do
+        url_version="$("${BUSYBOX}" wget -q --no-check-certificate -O - "${URL}" | grep -o 'alist_[^"]*' | sed 's/alist_//' | grep '_'"${ARCH}"'.deb$' | sed 's/_'"${ARCH}"'\.deb//' | tail -n 1)"
+        
+        if [ -n "$url_version" ]; then
+            break
+        fi
+        
+        ((retry_times++))
+        sleep 5s
+    done
     
     if [ -n "$url_version" ]; then
-        if version_ge "${url_version}" "${version}"; then
-            echo "[$(date "+%Y-%m-%d] [%H:%M:%S")] v${version}已是最新版本" >> "${MODDIR}/log.log"
-            sed -i "s/^version=.*/version=v${version}/g" "${MODDIR}/module.prop"
+        # 获取当前Alist版本号
+        if [ -x "${MODDIR}/bin/alist" ]; then
+            version="$("${MODDIR}/bin/alist" version | awk '/^Version:/ {print $2}')"
         else
-            echo "[$(date "+%Y-%m-%d] [%H:%M:%S")] v${version}版本较低，正在更新 ..." >> "${MODDIR}/log.log"
-            download_and_extract
-            copy_files
-            cleanup
-
-            max_retries=3
-            retry_count=0
-
-            while true; do
-                version=$("${MODDIR}/bin/alist" version | awk '/^Version:/ {print $2}')
-                if version_ge "${url_version}" "${version}"; then
-                    update_and_restart
-                    break
-                fi
-
-                sleep 5s
-                ((retry_count++))
-
-                if [[ ${retry_count} -eq ${max_retries} ]]; then
-                    handle_failed_update
-                    break
-                fi
-            done
+            version=""
         fi
+        check_and_update_version
     else
-        echo "[$(date "+%Y-%m-%d] [%H:%M:%S")] 更新失败未获取到版本号" >> "${MODDIR}/log.log"
+        echo "[$(date "+%Y-%m-%d] [%H:%M:%S")] Failed to detect version number for update failure detection !" >> "${MODDIR}/log.log"
     fi
     
     sleep 4h
