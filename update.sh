3974661a33f040d732e8466bcdeaca4e0dc9f2d8
sooop <88355765+Peooop@users.noreply.github.com>
2023-07-19 10:43:23 +0800

    更新 update.sh

--- update.sh
+++ update.sh
@@ -9,6 +9,7 @@ BUSYBOX=""
 
 # 更新的源
 URL="https://packages-cf.termux.dev/apt/termux-main/pool/main/a/alist/"
+
 # 架构
 ARCH=$(uname -m)
 
@@ -66,7 +67,7 @@ version_ge() {
 
 # 更新列表并重启进程
 update_and_restart() {
-    echo "web更新$(date "+%Y-%m-%d %H:%M:%S") 更新后v${version}版本" >> "${MODDIR}/log.log"
+    echo "[$(date "+%Y-%m-%d] [%H:%M:%S")] 更新后v${version}版本" >> "${MODDIR}/log.log"
     sed -i "s/^version=.*/version=v${version}/g" "${MODDIR}/module.prop"
     if pgrep -f 'alist' >/dev/null; then
         pkill alist 
@@ -76,7 +77,7 @@ update_and_restart() {
 
 # 处理更新失败
 handle_failed_update() {
-    echo "web更新$(date "+%Y-%m-%d %H:%M:%S") 更新失败" >> "${MODDIR}/log.log"
+    echo "[$(date "+%Y-%m-%d] [%H:%M:%S")] 更新失败" >> "${MODDIR}/log.log"
 }
 
 # 查找并设置busybox路径
@@ -94,34 +95,39 @@ while true; do
     
     # 获取当前Alist版本号
     version="$("${MODDIR}/bin/alist" version | awk '/^Version:/ {print $2}')"
+    
+    if [ -n "$url_version" ]; then
+        if version_ge "${url_version}" "${version}"; then
+            echo "[$(date "+%Y-%m-%d] [%H:%M:%S")] v${version}已是最新版本" >> "${MODDIR}/log.log"
+            sed -i "s/^version=.*/version=v${version}/g" "${MODDIR}/module.prop"
+        else
+            echo "[$(date "+%Y-%m-%d] [%H:%M:%S")] v${version}版本较低，正在更新 ..." >> "${MODDIR}/log.log"
+            download_and_extract
+            copy_files
+            cleanup
 
-    if version_ge "${url_version}" "${version}"; then
-        echo "web更新$(date "+%Y-%m-%d %H:%M:%S") v${version}已是最新版本" >> "${MODDIR}/log.log"
-        sed -i "s/^version=.*/version=v${version}/g" "${MODDIR}/module.prop"
+            max_retries=3
+            retry_count=0
+
+            while true; do
+                version=$("${MODDIR}/bin/alist" version | awk '/^Version:/ {print $2}')
+                if version_ge "${url_version}" "${version}"; then
+                    update_and_restart
+                    break
+                fi
+
+                sleep 5s
+                ((retry_count++))
+
+                if [[ ${retry_count} -eq ${max_retries} ]]; then
+                    handle_failed_update
+                    break
+                fi
+            done
+        fi
     else
-        echo "web更新$(date "+%Y-%m-%d %H:%M:%S") v${version}版本较低，正在更新 ..." >> "${MODDIR}/log.log"
-        download_and_extract
-        copy_files
-        cleanup
-
-        max_retries=3
-        retry_count=0
-
-        while true; do
-            version=$("${MODDIR}/bin/alist" version | awk '/^Version:/ {print $2}')
-            if version_ge "${url_version}" "${version}"; then
-                update_and_restart
-                break
-            fi
-
-            sleep 5s
-            ((retry_count++))
-
-            if [[ ${retry_count} -eq ${max_retries} ]]; then
-                handle_failed_update
-                break
-            fi
-        done
+        echo "[$(date "+%Y-%m-%d] [%H:%M:%S")] 更新失败未获取到版本号" >> "${MODDIR}/log.log"
     fi
+    
     sleep 4h
 done
