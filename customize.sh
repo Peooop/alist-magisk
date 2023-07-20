723e057c7e5113eb0357f8a2f38ebcf16e3b43d5
sooop <88355765+Peooop@users.noreply.github.com>
2023-07-20 08:54:14 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -3,7 +3,7 @@ MODDIR=${0%/*}
 
 ui_print "安装中，请稍等..."
 sleep 1
-ui_print "AList是一款提供多个储存挂载服务的开源软件"
+ui_print "AList是一款提供多个储存挂载服务的开源程序"
 ui_print "你可以挂载市面上大部分网盘如: 阿里云盘 百度网盘 蓝奏云等 使其便于管理"
 ui_print "Alist支持webdav协议使其可以桥接市面大部分观影阅读软件"
 ui_print "你可以尽情享受原画观影的快乐"
